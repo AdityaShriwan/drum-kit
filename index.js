fd8d799babab60bbdc1f442b5164ce41040777c1
AdityaShriwan <125428973+AdityaShriwan@users.noreply.github.com>
2023-02-14 21:08:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 
-//  make sound by button on html
 
-$(".drum").on("click", function () {
-var buttonPressed=this.innerHTML;
-$("h1").slidetoggle();
-
-setTimeout(function(){$("body").removeClass("newColor");}, 2000);
-makeSound(buttonPressed);
-buttonAnimation(buttonPressed);
-
-});
+ // click for mouse
+ $(".drum").on("click", function () {
+ var buttonClick=this.innerHTML;
+ setTimeout(function(){$("body").removeClass("newColor");}, 1000);
+ makeSound(buttonClick);
+ buttonAnimation(buttonClick);
+ });
 
 
-// make sound by keyboard key
-$(document).on("keydown", function(e){
-  var keyName=e.key;
-
+// click for keys
+  $(document).on("keydown", function(w){
+  var keyName=w.key;
   setTimeout(function(){$("body").addClass("newColor");}, 1000);
   makeSound(keyName);
   buttonAnimation(keyName);
 });
 
 
-// Make sound function
-function makeSound(buttonKey){
-  switch (buttonKey) {
+// sounds
+ function makeSound(buttonKey){
+ switch (buttonKey) {
     case "w":
           var tom1 = new Audio("sounds/tom-1.mp3");
           tom1.play();
@@ -49,19 +45,18 @@ function makeSound(buttonKey){
          var crash = new Audio("sounds/crash.mp3");
          crash.play();
          break;
-  case "l":
+    case "l":
          var kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-
-      break;
-    default:console.log(buttonKey);
-
-  }
+         kick.play();
+         break;
+         default:console.log(buttonKey);
+}
 }
 
-function buttonAnimation(keyValue){
-  var keyButton="."+keyValue;
-$(keyButton).addClass("pressed");
-  setTimeout(function(){$(keyButton).removeClass("pressed");}, 100);
 
+// buttonAnimation
+function buttonAnimation(keyValue){
+var keyButton="."+keyValue;
+$(keyButton).addClass("pressed");
+setTimeout(function(){$(keyButton).removeClass("pressed");}, 50);
 }
